9df2ad8293024b967457410e2f446fccabc380af
Ben Whalley <ben.whalley@plymouth.ac.uk>
2024-11-13 14:21:50 +0000

    function to create fake users

--- mindframe/silly.py
+++ mindframe/silly.py
@@ -1,4 +1,7 @@
 import random
+from mindframe.models import CustomUser
+from django.utils.text import slugify
+
 
 # Define syllables that sound like American sports stars or Vegas show names
 first_names = [
@@ -35,6 +38,17 @@ def silly_name():
     first_name = random.choice(first_names)
     last_name = random.choice(last_name_prefixes) + random.choice(last_name_suffixes)
     return f"{first_name} {last_name}"
+
+
+def silly_user():
+    sn = silly_name()
+    f, l = sn.split(" ")
+    return CustomUser.objects.create(
+        username=f"{slugify(sn)}{random.randint(1e4, 1e5)}",
+        first_name=f,
+        last_name=l,
+        is_active=False,
+    )
 
 
 if False:
