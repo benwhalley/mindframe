cad567ae8db3dd663dda06c285d7b892c109eb1d
Ben Whalley <ben.whalley@plymouth.ac.uk>
2024-11-24 13:02:04 +0000

    bump v

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'mindframe',
-    'version': '0.1.9',
+    'version': '0.1.9.1',
     'description': 'A Python package for the `mindframe` project',
     'long_description': 'None',
     'author': 'Ben Whalley',
