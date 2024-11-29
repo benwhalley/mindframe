62dd2a708a72360240f69230b36c2250ed8b05b3
Ben Whalley <ben.whalley@plymouth.ac.uk>
2024-11-29 19:31:31 +0000

    remove old settings

--- mindframe/settings.py
+++ mindframe/settings.py
@@ -5,32 +5,6 @@ from django.core.exceptions import ImproperlyConfigured
 from types import SimpleNamespace
 import os
 
-from sentence_transformers import SentenceTransformer
-
-
-class LazySentenceTransformer(object):
-    def __init__(self, model_name):
-        self.model_name = model_name
-        self._model = None
-
-    @property
-    def model(self):
-        if self._model is None:  # Load the model only when accessed
-            self._model = SentenceTransformer(self.model_name)
-        return self._model
-
-    def encode(self, *args, **kwargs):
-        # Proxy the call to the actual model's encode method
-        return self.model.encode(*args, **kwargs)
-
-
-MINDFRAME_AI_MODELS = getattr(
-    settings,
-    "MINDFRAME_AI_MODELS",
-    SimpleNamespace(
-        embedding=LazySentenceTransformer("sentence-transformers/all-MiniLM-L6-v2"),
-    ),
-)
 
 # 24 chars in alphapbet and 22 long = 24^22 so still very large for guessing but easier to read for humans
 MINDFRAME_SHORTUUID_ALPHABET = getattr(
