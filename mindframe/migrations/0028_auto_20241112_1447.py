5d91ef8d437f97ec69f7eab94753e1a5a1f32b8f
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-01-22 20:51:18 +0000

    .

--- mindframe/migrations/0028_auto_20241112_1447.py
+++ mindframe/migrations/0028_auto_20241112_1447.py
@@ -4,10 +4,19 @@ from pgvector.django import VectorExtension
 from django.db import migrations
 
 
+def f_():
+    # wrap VectorExtension because it fails if not superuser (we may want to install the extension prior to running the migration if in production)
+    try:
+        return VectorExtension()
+    except Exception as e:
+        print(e)
+        pass
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ("mindframe", "0027_adderrorlog"),
     ]
 
-    operations = [VectorExtension()]
+    operations = [f_()]
