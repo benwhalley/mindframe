a1eba6957746ab9beeeb7521eead8ac4959288c1
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-02-05 11:26:48 +0000

    ws

--- bin/database-managment.sh
+++ bin/database-managment.sh
@@ -2,9 +2,9 @@
 # dump and copy locally
 ssh az1 sudo docker exec -i postgres pg_dump -O -f mindframe.dump -F c -d mindframe -h 127.0.0.1 -p 5432 -U postgres && \
     ssh az1 sudo docker cp postgres:/mindframe.dump mindframe.dump && \
-    scp az1:mindframe.dump mindframe.dump  
-    
-    
+    scp az1:mindframe.dump mindframe.dump
+
+
 
 # restore
 
