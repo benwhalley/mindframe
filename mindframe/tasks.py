5cbb16cdd495377fb5187ef7b14b425bdb17fdab
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-02-06 11:14:59 +0000

    tidy

--- mindframe/tasks.py
+++ mindframe/tasks.py
@@ -5,16 +5,11 @@ from django.apps import apps
 
 logger = logging.getLogger(__name__)
 
-
 # @shared_task
-# def example_generate_embedding(example_id):
+# def example(id):
 #     """
-#     Async task to generate a text embedding for a given example.
+#     Async task to ...
 #     """
-#     Example = apps.get_model("mindframe", "Example")
-#     from mindframe.rag import embed_examples
-
-#     with transaction.atomic():
-#         example = Example.objects.filter(id=example_id)
-#         embed_examples(example)
-#         logger.debug(f"Generated embedding for example {example_id}")
+#     Model = apps.get_model("mindframe", "Model")
+#     a = Model.objects.get(id=id)
+#     a.do_something()
