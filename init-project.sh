64919e82469db4759c877e8c256d5d0c254f4f4a
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-02-06 12:38:29 +0000

    update install, remove migrations for simplicity

--- init-project.sh
+++ init-project.sh
@@ -5,25 +5,25 @@
 
 (echo "# \! AUTO GENERATED FILE DO NOT EDIT"; docker-compose -f compose/services.yml -f compose/development.yml -p mindframe config) > docker-compose.yml
 
-
 # build and start the containers
 docker-compose build base
 docker-compose up -d --build postgres redis
 sleep 1
-docker-compose up -d --build web chat worker
+docker-compose up --build web chat worker
 
+# in a new terminal window
 
 # setup database and import demo data
-docker exec -it mindframe-postgres-1 create-database.sh mindframe mindframe mfpassword
-docker exec -it mindframe-web-1 ./manage.py migrate
-docker exec -it mindframe-web-1 ./manage.py loaddata mindframe/fixtures/demo.json
+# replace mindframe-postgres-1 with running container name if different
+docker exec -it mindframe-postgres-1 create-database.sh mindframe_dbuser mindframe_db mfpassword
 
-# list databases
-docker exec -it mindframe-postgres-1 psql -U postgres -c "\l"
+
+docker-compose run web ./manage.py migrate
+docker-compose run web ./manage.py loaddata mindframe/fixtures/test.json
 
 # creates a user in mindframe with your current (shell) username
 # (change the password and email)
-docker exec -it mindframe-web-1 sh -c "DJANGO_SUPERUSER_PASSWORD=mfpassword ./manage.py createsuperuser --no-input --username=$(whoami) --email=test@example.com"
+docker-compose run web sh -c "DJANGO_SUPERUSER_PASSWORD=mfpassword ./manage.py createsuperuser --no-input --username=$(whoami) --email=test@example.com"
 
 # open the app in your browser at
 open http://127.0.0.1:8080/admin
