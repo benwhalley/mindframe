256dae55b0aae711f6fadc838d91487e8e80c23c
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-02-19 10:55:12 +0000

    .

--- mindframe/admin.py
+++ mindframe/admin.py
@@ -153,7 +153,10 @@ class ConversationAdmin(admin.ModelAdmin):
     save_on_top = True
     list_filter = (IsSyntheticFilter, "archived")
     inlines = [TurnInline]
-    list_display = ["__str__", "summary", "speakers", "active", "intervention"]
+    list_display = ["__str__", "summary", "speakers", "active", "n_turns", "intervention"]
+
+    def n_turns(self, obj):
+        return obj.turns.count()
 
     def speakers(self, obj):
         return ",".join(
