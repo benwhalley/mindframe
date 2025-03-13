98aa204befdd6e3641b883ab710ed62b46763e0d
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-03-13 11:11:50 +0000

    add script

---
+++ deploy/staging.sh
@@ -0,0 +1,28 @@
+cd ~/dev/mindframe
+git push staging main
+
+ssh az1 <<'EOF'
+cd ~/dev/mindframe-staging
+git reset
+(echo "# \! AUTO GENERATED FILE DO NOT EDIT"; \
+    CONTAINER_NAME_PREFIX=staging-mindframe \
+    WEB_PORT=9080 \
+    CHAT_PORT=9081 \
+    BASE_IMAGE_NAME="mindframe:staging" \
+    docker-compose \
+    -f compose/networks.yml \
+    -f compose/services.yml \
+    -f compose/mindframe.yml \
+    -f compose/constraints.yml \
+    -f compose/staging.yml \
+    config) > docker-compose.yml
+docker-compose build base
+docker-compose up --build -d web chat worker
+docker-compose run web \
+    ./manage.py migrate && \
+    ./manage.py register_telegram && \
+    ./manage.py setup_tasks
+
+
+docker-compose logs -f web chat worker
+EOF
