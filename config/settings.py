0ede8bb1457a18803ef45a49eddb48dedb3361d1
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-04-03 16:43:42 +0100

    switch to london time

--- config/settings.py
+++ config/settings.py
@@ -176,7 +176,7 @@ CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
 LANGUAGE_CODE = config("LANGUAGE_CODE", default="en-gb")
-TIME_ZONE = config("TIME_ZONE", default="UTC")
+TIME_ZONE = config("TIME_ZONE", default="Europe/London")
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
