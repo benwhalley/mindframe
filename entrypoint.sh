76e5e987cd7a320b7a65a7795ec32b4854c4790b
Ben Whalley <ben.whalley@plymouth.ac.uk>
2025-05-20 23:41:25 +0100

    deps

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,7 @@ case "$APP_TYPE" in
     exec python chatbot.py
     ;;
   "django")
-    echo "Starting Daphne"
-    # exec daphne -b 0.0.0.0 -p "$PORT" --verbosity 3 config.asgi:application
-    exec gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --workers "$WORKERS"
-    # gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --workers 3
-
+  exec gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --workers "$WORKERS"
     ;;
   *)
     echo "Unknown MINDFRAME_APP_TYPE: $APP_TYPE"
